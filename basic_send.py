4d46232b34b4f427a7dea9dba3db0936dd51e718
Aidan Gerber <agentaidan@gmail.com>
2021-09-19 08:08:29 -0500

    update settings

--- basic_send.py
+++ basic_send.py
@@ -1,5 +1,6 @@
 import os, django
 from twilio.rest import Client
+
 from findbestservery import main as fbs_main
 from findbestservery import maps_recommendations
 
@@ -10,36 +11,41 @@ from django.contrib.auth.models import User
 from main.models import Servery
 
 
-def main():
+
+account_sid = os.getenv('account_sid')
+auth_token = os.getenv('auth_token')
+msg_service_sid = os.getenv('msg_service_sid')
+def main(default_location):
 
     client = Client(account_sid, auth_token)
 
     max_value = -100
     correct_key = ''
-    print(maps_recommendations(User.objects.get(
-                username='Superuser').profile.default_location)[
-                0])
+    if len(default_location) == 0:
+        default_location = User.objects.get(
+            username='Superuser').profile.default_location
     for key, value in fbs_main(
-            maps_recommendations(User.objects.get(
-                username='Superuser').profile.default_location)[0][
+            maps_recommendations(default_location)[0][
                 'place_id']).items():
         if value > max_value:
             max_value = value
             correct_key = key
     current_dishes = Servery.objects.get(name=correct_key).current_dishes
-    message_string = f'Your recommended servery is {correct_key}'
+    message_string = f'Your recommended servery is {correct_key}. The meal for today is: '
     for dish in [{'dish': dish.dish.name, 'stars': dish.average_stars, 'reviews': dish.review_count}
                  for dish in current_dishes]:
-        message_string += f"\n{dish['dish']}, {dish['stars']}/5 with {dish['reviews']} reviews"
+        message_string += f"\n{dish['dish']}, {dish['stars']}/5"
+    message_string += f'\nIf you are not at your default location, {default_location}, reply with your current location'
 
     def send_message(message: str):
         message = client.messages.create(
             messaging_service_sid=msg_service_sid,
             body=message,
-            to='+19713525393'
+            to=os.getenv('phone_number')
         )
+
     send_message(message_string)
 
 
 if __name__ == '__main__':
-    main()
+    main('')
--- notification.py
+++ notification.py
@@ -4,13 +4,10 @@ from flask import Flask, request, Response, session
 from twilio.twiml.messaging_response import MessagingResponse
 from twilio.rest import Client
 from findbestservery import maps_recommendations
-
-account_sid = os.environ['TWILIO_ACCOUNT_SID']
-auth_token = os.environ['TWILIO_AUTH_TOKEN']
-msg_service_sid = os.environ['TWILIO_MSG_SERVICE_SID']
+from basic_send import main, account_sid, auth_token, msg_service_sid
 
 client = Client(account_sid, auth_token)
-gmaps = googlemaps.Client(key="AIzaSyBI58ny7OqVTmEKzqumFPHKlU9B4sgXkvw")
+gmaps = googlemaps.Client(key=os.getenv('GOOGLE_KEY'))
 app = Flask(__name__)
 app.secret_key = os.urandom(32)
 app.config.from_object(__name__)
@@ -59,12 +56,6 @@ def sms():
 
     message_body = request.form['Body'].strip()
 
-    if number in callers:
-        name = callers[number]
-    else:
-        name = "Random Person"
-
-    # probably connect number to name in database
     while not waiting_for_a_number:
         results = maps_recommendations(message_body)
         session["results"] = results
@@ -74,7 +65,7 @@ def sms():
             session['flag'] = waiting_for_a_number
         else:
             continue
-        msg = f"Hello {name}, based on your input here are the first few choices\n"
+        msg = f"Hello, based on your input here are the first few choices\n"
         for (index, result) in enumerate(results):
             msg += str(index + 1) + ". " + result + "\n"
         resp.message(msg)
@@ -83,14 +74,11 @@ def sms():
     if waiting_for_a_number:
         choice = int(message_body) - 1
         results = session.get("results")
-        if choice < len(results) and choice >= 0:
-            result = results[choice]
-        django_request = results[choice]["place_id"]
-        # print(django_request)
         waiting_for_a_number = False
         session['flag'] = waiting_for_a_number
         resp.message("We got your response. Your current location is: " + results[choice]["name"])
         # send it to django
+        main(results[choice]['name'])
     return str(resp)
 
 
